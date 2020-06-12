7707fe99c8a9aa0b88b86396008842945b6eabea
Mohit Vadsak <mohitvadsak107@gmail.com>
2020-06-12 15:45:43 +0530

    Updated Notices and Cast Vote

--- app/src/main/java/com/example/evoting/CastVote.java
+++ app/src/main/java/com/example/evoting/CastVote.java
@@ -2,8 +2,12 @@ package com.example.evoting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class CastVote extends AppCompatActivity {
 
@@ -12,6 +16,14 @@ public class CastVote extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cast_vote);
 
-
+        Button b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(CastVote.this,Dashboard.class);
+                startActivity(i);
+                Toast.makeText(getApplicationContext(),"You've voted successfully!",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
